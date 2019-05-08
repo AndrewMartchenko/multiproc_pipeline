5058326af456d7154986dbde8dbce70b4a563878
Andrew Martchenko <andrew.martchenko@gmail.com>
2019-05-08 14:07:18 +1000

    Refactoring to use abstract base class for stages

--- multiproc_pipe.py
+++ multiproc_pipe.py
@@ -1,5 +1,8 @@
 import multiprocessing as mp
 import types
+from abc import ABC, abstractmethod
+
+# TODO: add merge and split
 
 class MultiprocPipe:
     def __init__(self, head_stage, maxqsize=100):
@@ -10,26 +13,25 @@ class MultiprocPipe:
         while stage is not None:
             self.mp_list.append(mp.Process(target=stage.run))
             self.tail_stage = stage
+            if not isinstance(stage, PutterStage):
+                break
             stage = stage.next_stage
 
-        # Head process should not be no_output stage, because then the pipe would be no diferent to a si
-
-        if type(self.head_stage) is PipeStage or \
-           type(self.head_stage) is VoidStage:
+        if isinstance(self.head_stage, GetterStage):
             # Create input queue
             self.tx_q = mp.Queue(maxsize=maxqsize)
             self.head_stage.rx_q = self.tx_q
         else:
             self.tx_q = None
         
-        if type(self.tail_stage) is PipeStage or \
-           type(self.tail_stage) is GenStage:
+        if isinstance(self.tail_stage, PutterStage):
             # Create output queue
             self.rx_q = mp.Queue(maxsize=maxqsize)
             self.tail_stage.tx_q = self.rx_q
         else:
             self.rx_q = None
 
+    def start(self):
         # Start all the processes
         for p in self.mp_list:
             p.start()
@@ -61,21 +63,28 @@ def Stage(target, args=(), is_input=True, is_output=True, id=None):
     if not is_input and not is_output:
         return GenVoidStage(target, args, id)
 
-class PipeStage:
+
+class BaseStage(ABC):
     __stage_count = 0 # keep track of how many processes have been created
     def __init__(self, target, args=(), id=None):
         assert not is_lambda(target), 'target cannot be a lambda function.'
         self.target = target
         self.args = args
-        self.tx_q = None
-        self.rx_q = None
-        self.next_stage = None
 
         if id is None:
-            self.id = PipeStage.__stage_count
+            self.id = BaseStage.__stage_count
         else:
             self.id = id
-        PipeStage.__stage_count += 1
+        BaseStage.__stage_count += 1
+
+    @abstractmethod
+    def run(self):
+        pass
+
+class GetterStage(BaseStage):
+    def __init__(self, target, args=(), id=None):
+        BaseStage.__init__(self, target, args, id)
+        self.rx_q = None
 
     def get(self):
         res = self.rx_q.get()
@@ -83,17 +92,29 @@ class PipeStage:
             self.rx_q.close()
         return res
 
+class PutterStage(BaseStage):
+    def __init__(self, target, args=(), id=None):
+        BaseStage.__init__(self, target, args, id)
+        self.tx_q = None
+        self.next_stage = None
+
     def put(self, val):
         self.tx_q.put(val)
 
     def link(self, next_stage, maxqsize=100):
-        if type(next_stage) is GenStage:
-            raise TypeError(f'Cannot link process {self.id} to generator process {next_stage.id}.')
+        if not isinstance(next_stage, GetterStage):
+            raise TypeError(f'Cannot link process {self.id} to a non-getter stage {next_stage.id}.')
 
         self.next_stage = next_stage
         self.tx_q = mp.Queue(maxqsize)
         self.next_stage.rx_q = self.tx_q
         return self
+    
+
+class PipeStage(GetterStage, PutterStage):
+    def __init__(self, target, args=(), id=None):
+        GetterStage.__init__(self, target, args, id)
+        PutterStage.__init__(self, target, args, id) # Base class constructor will be called twice
 
     def run(self):
         while True:
@@ -105,21 +126,9 @@ class PipeStage:
             self.put(y)
         print(f'stage {self.id} done')
 
-    def _raise_type_error(self, method_name):
-        raise TypeError(f'"{method_name}" method is not definded for object of type "{type(self).__name__}"')
-        
-
-        
-class VoidStage(PipeStage):
+class VoidStage(GetterStage):
     def __init__(self, target, args=(), id=None):
-        PipeStage.__init__(self, target, args, id)
-        del(self.tx_q)
-
-    def put(self, val):
-        self._raise_type_error('put')
-
-    def link(self, next_stage, maxqsize=100):
-        self._raise_type_error('link')
+        GetterStage.__init__(self, target, args, id)
 
     def run(self):
         while True:
@@ -129,16 +138,9 @@ class VoidStage(PipeStage):
             self.target(x, *self.args)
         print(f'stage {self.id} done')
 
-class GenStage(PipeStage):
+class GenStage(PutterStage):
     def __init__(self, target, args=(), id=None):
-        PipeStage.__init__(self, target, args, id)
-        self.tx_q = None
-        self.next_stage = None
-        # This stage cannot receive inputs. rx_q not required
-        del(self.rx_q)
-
-    def get(self):
-        self._raise_type_error('get')
+        PutterStage.__init__(self, target, args, id)
 
     def run(self):
         while True:
@@ -148,21 +150,9 @@ class GenStage(PipeStage):
                 break
         print(f'stage {self.id} done')
 
-class GenVoidStage(PipeStage):
-    """This class is only here for completness"""
+class GenVoidStage(BaseStage):
     def __init__(self, target, args=(), id=None):
-        PipeStage.__init__(self, target, args, id)
-        self.tx_q = None
-        self.next_stage = None
-        # This stage cannot receive inputs. rx_q not required
-        del(self.rx_q)
-        del(self.tx_q)
-
-    def get(self):
-        self._raise_type_error('get')
-
-    def put(self, val):
-        self._raise_type_error('put')
+        BaseStage.__init__(self, target, args, id)
 
     def run(self):
         while True:
@@ -207,12 +197,14 @@ def void_func(x, *args):
 
 if __name__ == '__main__':
 
-    
+    # Test only one GenVoid pipline/stage
     gen_void = GenVoid(10)
-    px = Stage(target=gen_void.work, is_input=False, is_output=False, id='gen_void_1')
-    pipe = MultiprocPipe(px)
+    p0 = Stage(target=gen_void.work, is_input=False, is_output=False, id='gen_void_1')
+    pipe = MultiprocPipe(p0)
 
-    
+    print('Test 1 done.')
+
+    # Test Geng -> Pipe -> Void pipeline
     gen = GenGen(10)
     p0 = Stage(target=gen.work, is_input=False)
     p1 = Stage(target=func, args=(2,))
@@ -221,8 +213,46 @@ if __name__ == '__main__':
     p4 = Stage(target=func, args=(2,))
     p5 = Stage(target=void_func, args=(3,), is_output=False)
 
-    p0.link(p1.link(p2.link(p3.link(p4.link(p5)))))
-    # TODO: add merge and split
+    
+    p0.link(p1)
+    p1.link(p2)
+    p2.link(p3)
+    p3.link(p4)
+    p4.link(p5)
+
+    pipe = MultiprocPipe(p0)
+    pipe.start()
+    print('Test 2 done.')
+
+    # Test Pipe -> Pipe -> Pipe pipeline
+    p1 = Stage(target=func, args=(2,))
+    p2 = Stage(target=func, args=(2,))
+    p3 = Stage(target=func, args=(2,))
+    p4 = Stage(target=func, args=(2,))
+
+    p1.link(p2)
+    p2.link(p3)
+    p3.link(p4)
+
+    pipe = MultiprocPipe(p1)
+    pipe.start()
+    
+    # Put values in pipe
+    for i in range(10):
+        print('input:', i)
+        pipe.put(i)
+    pipe.put(None)
+
+    print('Test 3 done.')
+
+    # Get results from pipe
+    while True:
+        v = pipe.get()
+        # print('output:', v)
+        if v is None:
+            break
+
+
 
     # This is what split should look like
     # p0.link(p1a.link(p1b.link(p1c)),
@@ -231,21 +261,6 @@ if __name__ == '__main__':
 
     # This is what merge would look like
     # p4.merge(p1c, p2c, p3c)
-    
-
-    pipe = MultiprocPipe(p0)
-
-    # # Put values in pipe
-    # for i in range(10):
-    #     print('input:', i)
-    #     pipe.put(i)
-    # pipe.put(None)
 
 
-    # # Get results from pipe
-    # while True:
-    #     v = pipe.get()
-    #     # print('output:', v)
-    #     if v is None:
-    #         break
 
